5aff75fc8b8d4abf4eefbd8717e7366e32a7a5bf
tian <tian@localhost.localdomain>
2026-06-23 15:23:02 +0800

    fix(app): provide LocalRepository in widget tests, add pump duration

--- app/test/widget/full_flow_test.dart
+++ app/test/widget/full_flow_test.dart
@@ -1,16 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:brickfinder/main.dart';
+import 'package:brickfinder/pages/home/home_page.dart';
 import 'package:brickfinder/api/api_client.dart';
+import 'package:brickfinder/repository/local_repository.dart';
 
 void main() {
   testWidgets('app starts and shows home page', (tester) async {
+    final repo = LocalRepository();
+    await repo.init();
     await tester.pumpWidget(
       MultiProvider(
         providers: [
+          Provider(create: (_) => repo),
           Provider(create: (_) => ApiClient(baseUrl: 'http://test')),
         ],
-        child: const BrickFinderApp(),
+        child: const MaterialApp(home: HomePage()),
       ),
     );
     await tester.pump();

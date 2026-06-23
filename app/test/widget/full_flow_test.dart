d1c1dbe76828372fa521e7ba4a5218e407118b87
tian <tian@localhost.localdomain>
2026-06-23 20:20:31 +0800

    fix(app): move imports before class in full_flow_test

--- app/test/widget/full_flow_test.dart
+++ app/test/widget/full_flow_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:brickfinder/pages/home/home_page.dart';
 import 'package:brickfinder/repository/local_repository.dart';
 import 'package:brickfinder/models/inventory_session.dart';
@@ -10,9 +12,6 @@ class FakeRepo extends LocalRepository {
   @override
   Future<List<InventorySession>> getAllSessions() async => [];
 }
-
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('app starts and shows title', (tester) async {
